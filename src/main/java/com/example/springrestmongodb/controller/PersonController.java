35d76ebbdf23f7efca3491df2801d61c28ef4a73
niravassar-hitrust <nirav.assar@hitrustalliance.net>
2023-07-20 16:01:17 -0500

    delete import

--- src/main/java/com/example/springrestmongodb/controller/PersonController.java
+++ src/main/java/com/example/springrestmongodb/controller/PersonController.java
@@ -3,7 +3,6 @@ package com.example.springrestmongodb.controller;
 import com.example.springrestmongodb.data.Person;
 import com.example.springrestmongodb.data.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
